5748a76ae957b528dedd4026e438a4b5a68f7c22
David Basden <davidb@anchor.net.au>
2011-08-29 13:04:53 +1000

    visualisation of unresolved dependencies

--- lib/vis.py
+++ lib/vis.py
@@ -15,7 +15,28 @@ def write_dot_from_items(items, outfile):
 			print '\t\t"%s" -> "%s";' % (item.name, dep.name)
 	print >> outfile, '}'
 
+def write_dot_from_itemdata(items, outfile, ignore_completed=False):
+	'''generate dot file output for dependencies amongst supplied items
+	writes output to a supplied file already opened for writing.
+
+	Does not have any idea what a group is and will treat it just like anything else
+	'''
+	if ignore_completed:
+		completed = set(item['name'] for item in items if item['state'] == 'COMPLETE')
+	print >> outfile, 'digraph depgraph {'
+	for item in items:
+		if ignore_completed and item['name'] in completed:
+			continue
+		print '\tname = "%s";' % (item['name'],)
+		for dep in item.get('depends',[]):
+			if ignore_completed and dep in completed:
+				continue
+			print '\t\t"%s" -> "%s";' % (item['name'], dep)
+	print >> outfile, '}'
 
 if __name__ == '__main__':
 	import sys
-	write_dot_from_items([], sys.stdout)
+	import json
+	# Take input from task json on stdin
+	taskdata = json.load(sys.stdin)
+	write_dot_from_itemdata(taskdata['items'], sys.stdout, ignore_completed=True)
